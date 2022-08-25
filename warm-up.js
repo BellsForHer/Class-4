d0fdcd4dcdb3180ce75ae359200eafbb14bd78de
BellsForHer <arhall88@gmail.com>
2022-08-25 17:59:31 -0500

    Update warm-up.js

--- warm-up.js
+++ warm-up.js
@@ -1,10 +1,25 @@
-let names = ['Stacy', 'Waldo', 'John'];
+findWaldo (['Stacy', 'Waldo', 'John']);
+findWaldo(['Waldo', 'Jimmy', 'James', 'Jamie', 'Waldo']);
+let newArr = [];
 
-function findWaldo (names) {
-    if (names === 'Waldo') {
-        return 'Found Waldo';
-    } else {
-        return 'Not Waldo';
+// function findWaldo (group) {
+//     if (group === 'Waldo') {
+//         return 'Found Waldo';
+//     } else {
+//         return 'Not Waldo';
+//     }
+//     };
+//     console.log('Waldo');
+
+function findWaldo (group) {
+    for(let i=0; i < group.length; i++) {
+        if (group[i]==='Waldo') {
+            newArr.push("I found Waldo!");
+        } else {
+            newArr.push(group[i]);
+        }
+
     }
-    };
-    console.log('Waldo');
+
+    return newArr
+}
