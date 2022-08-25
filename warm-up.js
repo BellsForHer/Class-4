b9535da31d8d5d2a1bcf31350a01f49c6e9c9a48
BellsForHer <arhall88@gmail.com>
2022-08-25 17:40:45 -0500

    Initial commit

---
+++ warm-up.js
@@ -0,0 +1,10 @@
+let names = ['Stacy', 'Waldo', 'John'];
+
+function findWaldo (names) {
+    if (names === 'Waldo') {
+        return 'Found Waldo';
+    } else {
+        return 'Not Waldo';
+    }
+    };
+    console.log('Waldo');
