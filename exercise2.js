ab450db6ec53ff48bbb6555628b0070825732b9f
BellsForHer <arhall88@gmail.com>
2022-08-29 20:29:50 -0500

    Create exercise2.js

---
+++ exercise2.js
@@ -0,0 +1,10 @@
+countTrue([true, false, false, true, false]) 
+
+countTrue([false, false, false, false]) 
+
+countTrue([]) 
+
+function countTrue (group) {
+    for(let i=0; i < group.length; i++) 
+    
+}
